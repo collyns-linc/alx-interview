0199839f330b38e624e5551e0b54b186d10c3b76
Collins <coduor24@gmail.com>
2024-12-29 16:58:22 +0300

    fix pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,6 +4,8 @@
 Pascal triangle solution that takes a n argument
 and returns the nth row of the Pascal triangle.
 """
+
+
 def pascal_triangle(n):
     """
     Pascal triangle solution that takes a n argument
@@ -14,8 +16,9 @@ def pascal_triangle(n):
         my_list.append([1])
         for i in range(1, n):
             x = my_list[i - 1]
-            my_list.append([1] + [x[j] + x[j + 1] for j in range(len(x) - 1)] + [1])
-                
+            my_list.append([1] + [x[j] + x[j + 1]
+                                  for j in range(len(x) - 1)] + [1])
+
         return my_list
     else:
         return my_list
